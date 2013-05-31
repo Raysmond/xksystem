32d52dc902eec148592a067caa8147a8f26cab31
Raysmond <jiankunlei@126.com>
2013-05-31 20:41:54 +0800

    add test cases

--- test/com/raysmond/hibernate/TestICourseController.java
+++ test/com/raysmond/hibernate/TestICourseController.java
@@ -2,6 +2,9 @@ package com.raysmond.hibernate;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,38 +16,62 @@ public class TestICourseController extends HibernateBaseTest {
 
 	@Autowired
 	ICourseController courseController;
-	
+
 	@Test
-	public void testRegisterTermCourse(){
-		//create a term
+	public void testRegisterTermCourse() {
+		// create a term
 		Term term = Term.create(2012, ChooseCourseStatus.NOT_STARTED,
 				ConcreteTerm.ONE, getPersistenceManager());
-		
-		//create a teacher
-		Teacher teacher = Teacher.create("OOTeacher","1111", getPersistenceManager());
-		
-		TermCourse course = courseController.registerTermCourse(
-				"OOT_2012", "Z2207", 50, teacher, term);
+
+		// create a teacher
+		Teacher teacher = Teacher.create("OOTeacher", "1111",
+				getPersistenceManager());
+
+		TermCourse course = courseController.registerTermCourse("OOT_2012",
+				"Z2207", 50, teacher, term);
 		this.assertObjectPersisted(course);
 	}
-	
+
 	@Test
-	public void testStartChoosingCourse(){
-		fail("not implemented yet");
-	} 
-	
-	@Test
-	public void testEndChoosingCourse(){
+	public void testStartChoosingCourse() {
 		fail("not implemented yet");
 	}
-	
+
 	@Test
-	public void testPrepareChooseCourse(){
+	public void testEndChoosingCourse() {
 		fail("not implemented yet");
 	}
-	
+
 	@Test
-	public void testAjustCourseStudentLimit(){
+	public void testPrepareChooseCourse() {
 		fail("not implemented yet");
+	}
+
+	@Test
+	public void testAjustCourseStudentLimit() {
+		// create a term
+		Term term = Term.create(2012, ChooseCourseStatus.NOT_STARTED,
+				ConcreteTerm.ONE, getPersistenceManager());
+
+		// create a teacher
+		Teacher teacher = Teacher.create("OOTeacher", "1111",
+				getPersistenceManager());
+
+		// save course
+		TermCourse course = courseController.registerTermCourse("OOT_2012",
+				"Z2207", 50, teacher, term);
+		this.assertObjectPersisted(course);
+
+		// ajust students limit from 50 to 60
+		courseController.ajustCourseStudentLimit(course, 60);
+
+		// find the course from DB and assert students limit
+		String hql = "from Course c where c.id=:id";
+		Query query = getPersistenceManager().createQuery(hql).setParameter(
+				"id", course.getId());
+		List<TermCourse> courses = query.list();
+		
+		assertEquals(1, courses.size());
+		assertEquals(60, courses.get(0).getStudentsLimit().intValue());
 	}
 }
